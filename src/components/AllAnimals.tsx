d826f788c56e43f10c9f4702a499d2aba0ecb102
Sandra S <zandrastr@hotmail.com>
2023-08-22 20:11:17 +0200

    handle api vs local storage and use navigate

--- src/components/AllAnimals.tsx
+++ src/components/AllAnimals.tsx
@@ -1,39 +1,51 @@
 import { ReactEventHandler, useEffect, useState } from 'react';
 import { IAnimal } from '../models/IAnimal';
 import { getAllAnimals } from '../services/ApiResponseService';
+import { useNavigate } from 'react-router-dom';
 
 export const AllAnimals = () => {
   const [animals, setAnimals] = useState<IAnimal[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getAnimals = async () => {
-      try {
-        const allAnimalsData: IAnimal[] | undefined = await getAllAnimals();
-        if (allAnimalsData) {
-          setAnimals(allAnimalsData);
+      const animalsFromLocalStorage = localStorage.getItem('allAnimals');
+
+      if (!animalsFromLocalStorage) {
+        try {
+          const animalsFromApi: IAnimal[] | undefined = await getAllAnimals();
+          if (animalsFromApi) {
+            const allAnimals = JSON.stringify(animalsFromApi);
+            localStorage.setItem('allAnimals', allAnimals);
+            setAnimals(animalsFromApi);
+          }
+        } catch (error) {
+          console.log('Error getting data from API:', error);
         }
-      } catch (error) {
-        console.log('Error getting data from API:', error);
+      } else {
+        const allAnimals = JSON.parse(animalsFromLocalStorage);
+        setAnimals(allAnimals);
       }
     };
     getAnimals();
   }, []);
-
-  const animalsArrayToString = JSON.stringify(animals);
-  localStorage.setItem('allAnimals', animalsArrayToString);
 
   const handleImgError: ReactEventHandler<HTMLImageElement> = (event) => {
     const target = event.target as HTMLImageElement;
     target.src = './src/images/no-image-available.jpg';
   };
 
+  const handleClick = (id: number) => {
+    navigate(`/animals/${id}`);
+  };
+
   return (
     <>
       <h1>Alla djur</h1>
       {animals.map((oneAnimal) => (
-        <div key={oneAnimal.id}>
+        <div key={oneAnimal.id} onClick={() => handleClick(oneAnimal.id)}>
           <h2>{oneAnimal.name}</h2>
-          <img src={oneAnimal.imageUrl} alt={oneAnimal.shortDescription} width='250' onError={handleImgError} />
+          <img src={oneAnimal.imageUrl} alt={oneAnimal.latinName} width='250' onError={handleImgError} />
         </div>
       ))}
     </>
