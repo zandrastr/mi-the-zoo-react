33baacb40ba45281143560c1dc1c5d743a8b041b
Sandra S <zandrastr@hotmail.com>
2023-08-22 20:08:08 +0200

    add router path to direct to one animal component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from './components/Home';
 import { AllAnimals } from './components/AllAnimals';
+import { OneAnimal } from './components/OneAnimal';
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: '/animals',
     element: <AllAnimals></AllAnimals>,
+  },
+  {
+    path: '/animals/:id',
+    element: <OneAnimal></OneAnimal>,
   },
 ]);
