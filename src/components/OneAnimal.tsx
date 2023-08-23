43d0fdbf8ab2a387a9035200b2004310b1aa8f71
Sandra S <zandrastr@hotmail.com>
2023-08-23 21:59:05 +0200

    add feed button and update feeding data

--- src/components/OneAnimal.tsx
+++ src/components/OneAnimal.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { IAnimal } from '../models/IAnimal';
-import { useEffect, useState } from 'react';
+import { ReactEventHandler, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export interface IOneAnimalProps {
   id: number;
@@ -19,24 +20,81 @@ export const OneAnimal = () => {
       const foundAnimalById = dataInLocalStorageParsed.find((animal) => animal.id === Number(id));
 
       if (foundAnimalById) {
-        console.log('Found animal by ID:', foundAnimalById.id);
         setCurrentAnimal(foundAnimalById);
       } else console.log('Error retrieving animal by ID.');
     }
   }, [id]);
 
+  const handleClick = () => {
+    console.log('Button to feed pressed.');
+    let animalIndex;
+    const now = new Date();
+
+    let updatedCurrentAnimal: IAnimal = {
+      id: 0,
+      imageUrl: '',
+      isFed: false,
+      lastFed: '',
+      latinName: '',
+      longDescription: '',
+      medicine: '',
+      name: '',
+      shortDescription: '',
+      yearOfBirth: 0,
+    };
+
+    updatedCurrentAnimal = { ...currentAnimal!, isFed: true, lastFed: now.toISOString() };
+    setCurrentAnimal(updatedCurrentAnimal);
+
+    const dataInLocalStorage: string | null = localStorage.getItem('allAnimals');
+
+    if (dataInLocalStorage) {
+      const dataInLocalStorageParsed: IAnimal[] = JSON.parse(dataInLocalStorage);
+
+      dataInLocalStorageParsed.find((animal, i) => {
+        if (animal.id === currentAnimal!.id) {
+          animalIndex = i;
+        }
+      });
+
+      const newAnimalArray = [...dataInLocalStorageParsed!.slice(0, animalIndex!), updatedCurrentAnimal, ...dataInLocalStorageParsed!.slice(animalIndex! + 1)];
+
+      localStorage.removeItem('allAnimals');
+      localStorage.setItem('allAnimals', JSON.stringify(newAnimalArray));
+    }
+  };
+
+  const handleImgError: ReactEventHandler<HTMLImageElement> = (event) => {
+    const target = event.target as HTMLImageElement;
+    target.src = '../src/images/no-image-available.jpg';
+  };
+
+  const navigate = useNavigate();
+
+  const handleGoBackBtnClick = () => {
+    navigate('/');
+  };
+
   return (
     <>
       {currentAnimal && (
-        <div>
+        <div className='animalInfoWrapper'>
           <h1>{currentAnimal.name}</h1>
           <p>{currentAnimal.shortDescription}</p>
-          <img src={currentAnimal.imageUrl} alt={currentAnimal.latinName} width='250'></img>
+          <img src={currentAnimal.imageUrl} alt={currentAnimal.latinName} width='250' onError={handleImgError} />
           <p>Född: {currentAnimal.yearOfBirth}</p>
           <p>Mediciner: {currentAnimal.medicine}</p>
-          <p>Senast matad: {currentAnimal.lastFed}</p>
+          <div className={(currentAnimal.isFed ? 'greenFeedingStatus' : 'redFeedingStatus') + ' feedingInfo'}>
+            <p>Senast matad: {currentAnimal.lastFed}</p>
+            <button className='feedBtn' disabled={currentAnimal.isFed} onClick={handleClick}>
+              Mata djur
+            </button>
+          </div>
           <p>Latinskt namn: {currentAnimal.latinName}</p>
           <p>Fakta: {currentAnimal.longDescription}</p>
+          <button className='goBackBtn' onClick={handleGoBackBtnClick}>
+            Tillbaka
+          </button>
         </div>
       )}
     </>
